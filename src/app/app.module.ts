e6111bdfdbaf1eeba69ca0c91dd41c47cbe8ee37
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2022-02-13 22:51:58 -0300

    weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { allAppRoutes } from './router';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(allAppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
